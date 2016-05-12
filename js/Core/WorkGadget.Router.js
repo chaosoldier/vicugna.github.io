cb71bc3a8cd36b7fb3f58380ee6f946cdf74b798
yuta uehara <yuta.uehara@bizreach.co.jp>
2016-05-12 10:22:48 +0900

    システムテストリリース

---
+++ js/Core/WorkGadget.Router.js
@@ -0,0 +1,53 @@
+/*global WorkGadget:true, Backbone, $ */
+
+var WorkGadget = WorkGadget || {};
+
+(function () {
+	'use strict';
+
+	// Handles a single dynamic route to show
+	// the active vs complete todo items
+	WorkGadget.Router = Backbone.Marionette.AppRouter.extend({
+		appRoutes: {
+			 '': 'SendReport',
+			 'Send': 'SendReport',
+			 'Read': 'ReadReport'
+		}
+	});
+	
+	// Controller is just Object
+	// For store routing inner logic
+	WorkGadget.Controller = Backbone.Marionette.Object.extend({
+
+		initialize: function () {
+			WorkGadget.App.View.Header = new WorkGadget.View.Header();
+		},
+
+		ObserveHash: function(){
+			WorkGadget.App.View.Header.navTo(Backbone.history.getFragment());
+		},
+
+		SendReport: function(){
+			this.ObserveHash();
+
+			var content = new WorkGadget.View.SendReport({
+						model: new WorkGadget.Model.SendReport()
+					})
+
+			WorkGadget.App.View.Root.showChildView('main', content);
+		},
+
+		ReadReport: function(){
+			this.ObserveHash();
+			
+			var reports = new Backbone.Collection();
+			var content = new WorkGadget.View.ReadReport({collection : reports})
+			WorkGadget.Model.ReadReportItems(new Date(), function (report){
+				reports.add(report);
+			});
+			
+			WorkGadget.App.View.Root.showChildView('main', content);
+		},
+
+ 	});
+})();
---
+++ js/ReadReport/WorkGadget.View.ReadReport.js
@@ -0,0 +1,98 @@
+/*global WorkGadget:true, Backbone */
+
+var WorkGadget = WorkGadget || {};
+
+(function () {
+	'use strict';
+
+	WorkGadget.View = WorkGadget.View || {};
+
+	//Read Repoert View
+	// ------------------
+	WorkGadget.View.ReadReportItem = Backbone.Marionette.ItemView.extend({
+		
+		template: '#template-ReadReportItem',
+
+	})
+
+	WorkGadget.View.ReadReport = Backbone.Marionette.CompositeView.extend({
+		
+		template: '#template-ReadReport',
+
+		ui: {
+			toggleButton: "#ToggleButton",
+			calendar: ".input-group.date",
+			calendarInput: ".input-group.date input",
+		},
+
+		events: {
+			"change @ui.calendarInput": "changeDate",
+			"click @ui.toggleButton": "togglePanel",
+		},
+
+		collectionEvents: {
+        	change: 'render'
+    	},
+
+		childView: WorkGadget.View.ReadReportItem,
+		childViewContainer: '#ReportContainer',
+		
+		initialize: function (){
+			this.bindUIElements();
+
+			
+		},
+
+		onShow: function () {
+	        // Invoke the datetimepicker plugin
+      	 	this.setCalendar();
+	    },
+
+		onClose: function () {
+        	// Destroy the datetimepicker plugin
+        	this.ui.calendar.datepicker('destroy');
+    	},
+
+		setCalendar: function(){
+
+			this.ui.calendar.datepicker({
+			    todayBtn: "linked",
+			    orientation: "bottom auto",
+			    keyboardNavigation: false,
+			    daysOfWeekDisabled: "0,6",
+			    autoclose: true,
+			    todayHighlight: true,
+			    format: "yyyy/mm/dd",
+			    endDate: "today",
+			});
+		},
+
+		changeDate: function(){
+			var $this = this;
+			this.collection.reset()
+			var date = this.ui.calendarInput.val()
+
+			WorkGadget.Model.ReadReportItems(date, function (report){
+				$this.collection.add(report);
+			});
+		},
+
+		togglePanel: function(){
+			var expand = "glyphicon-resize-full";
+			var shrink = "glyphicon-resize-small";
+			var $icon = this.ui.toggleButton.find("span");
+
+			if ($icon.hasClass(expand)){
+				//state is shrink, so change to Show
+				$icon.removeClass(expand).addClass(shrink);
+				$(this.childViewContainer + " .collapse").collapse("show");
+			} else {
+				//state is expand, so change to Hide
+				$icon.removeClass(shrink).addClass(expand);
+				$(this.childViewContainer + " .collapse").collapse("hide");
+			}
+			
+		},
+
+	});
+})();
